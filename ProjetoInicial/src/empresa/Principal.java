0018793a210afbdf762801163beb67e5a99c6dfa
Eduardo Kunzel Teixeira <eduardo.kunzel@hotmail.com>
2024-02-20 09:32:01 -0300

    projeto inicial

---
+++ ProjetoInicial/src/empresa/Principal.java
@@ -0,0 +1,12 @@
+package empresa;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello Brazil!");
+        
+        
+    }
+    
+}
